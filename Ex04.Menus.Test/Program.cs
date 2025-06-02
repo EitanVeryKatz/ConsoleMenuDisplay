7e3ca1ed0e916cec2475532bad4d95fa60eccc97
Adi <Aditapiro12@gmail.com>
2025-06-02 12:27:40 +0300

    Made Program Main more convenient

--- Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Test/Program.cs
@@ -12,22 +12,45 @@ namespace Ex04.Menus.Test
     {
         public static void Main()
         {
-            //Functions functions = new Functions();
-            //MainMenu mainMenu = new MainMenu("Main Menu", functions);
-
-            //mainMenu.AddSubMenu("Sub Menu 1");
-            //mainMenu.AddSubMenu("Sub Menu 2");
-            //mainMenu.EnterSubMenu("Sub Menu 1");
-            //mainMenu.AddMenuItem("Item 1");
-            //mainMenu.AddMenuItem("Item 2");
-            //mainMenu.ResetToDefaultMenu();
-            //mainMenu.EnterSubMenu("Sub Menu 2");
-            //mainMenu.AddMenuItem("Item 3");
-            //mainMenu.ResetToDefaultMenu();
-            //mainMenu.Show();
-
-
-
+            MenuSystemTest();
+        }
+        public static void MenuSystemTest()
+        {
+            Console.WriteLine("Welcome to the Menu System Test!");
+            Console.WriteLine("Please choose a menu system to test:\n1- interface\n2- events");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                testInterfaces();
+            }
+            else if (choice == 2)
+            {
+                testEvents();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Exiting.");
+            }
+        }
+        public static void testInterfaces()
+        {
+            Console.WriteLine("Testing Interfaces Menu System...");
+            Functions functions = new Functions();
+            MainMenu mainMenu = new MainMenu("Main Menu", functions);
+            mainMenu.AddSubMenu("Sub Menu 1");
+            mainMenu.AddSubMenu("Sub Menu 2");
+            mainMenu.EnterSubMenu("Sub Menu 1");
+            mainMenu.AddMenuItem("Item 1");
+            mainMenu.AddMenuItem("Item 2");
+            mainMenu.ResetToDefaultMenu();
+            mainMenu.EnterSubMenu("Sub Menu 2");
+            mainMenu.AddMenuItem("Item 3");
+            mainMenu.ResetToDefaultMenu();
+            mainMenu.Show();
+        }
+        public static void testEvents()
+        {
+            Console.WriteLine("Testing Events Menu System...");
             Events.MainMenu mainMenu = new Events.MainMenu("Main Menu");
             mainMenu.AddSubMenu("Sub Menu 1");
             mainMenu.AddSubMenu("Sub Menu 2");
